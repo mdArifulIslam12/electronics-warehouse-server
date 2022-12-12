4b85e38498445831c75046670a0e3c99120d22bf
mdArifulIslam12 <mdarif767778@gmail.com>
2022-12-12 13:05:19 +0600

    server

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dq6qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const uri = "mongodb+srv://inventorybd:Z12BoMKnmAZraVNV@cluster0.dq6qt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
     await client.connect();
